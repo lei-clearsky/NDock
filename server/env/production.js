00edc2028462427f912fc0ae3b60adaeb38b5855
lei-clearsky <sportzhulei@gmail.com>
2018-01-04 22:37:39 -0500

    Update typo on JWT secret key

--- server/env/production.js
+++ server/env/production.js
@@ -1,6 +1,6 @@
 module.exports = {
   DATABASE_URI: process.env.MONGOLAB_URI,
-  SECRET: process.env.SESSION_SECRET,
+  SECRET: process.env.SECRET,
   TWITTER: {
     consumerKey: process.env.TWITTER_CONSUMER_KEY,
     consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
